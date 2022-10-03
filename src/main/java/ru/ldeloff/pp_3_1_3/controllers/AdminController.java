cb8c2757080fcb8440abc7d68041e2b656a8dceb
L_DelOff <51275636+LDelOff@users.noreply.github.com>
2022-10-03 23:37:32 +0300

    переделка под новую систему страниц

--- src/main/java/ru/ldeloff/pp_3_1_3/controllers/AdminController.java
+++ src/main/java/ru/ldeloff/pp_3_1_3/controllers/AdminController.java
@@ -8,6 +8,8 @@ import ru.ldeloff.pp_3_1_3.models.User;
 import ru.ldeloff.pp_3_1_3.service.RoleService;
 import ru.ldeloff.pp_3_1_3.service.UserService;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
@@ -20,61 +22,56 @@ public class AdminController {
     }
 
     @GetMapping(value = "")
-    public String AdminPage(Model model) {
-        return "admin/index";
-    }
-
-    @GetMapping(value = "/allUsers")
-    public String AllUsers(Model model) {
+    public String AdminPage(Principal principal, Model model) {
+        model.addAttribute("person", userService.getByEmail(principal.getName()));
         model.addAttribute("roles", roleService.getAllRoles());
         model.addAttribute("users", userService.getAllUser());
-        return "/admin/allUsers";
-    }
-
-    @GetMapping(value = "/new")
-    public String newUser(Model model) {
-        model.addAttribute("person", new User());
-        model.addAttribute("roles", roleService.getAllRoles());
-        return "admin/new";
+        model.addAttribute("newUser", new User());
+        model.addAttribute("userUp", new User());
+        return "panelAdmin";
     }
 
     @PostMapping(value = "/add")
-    public String add(@RequestParam(name = "username") String username,
+    public String add(@RequestParam(name = "firstName") String firstName,
+                      @RequestParam(name = "lastName") String lastName,
+                      @RequestParam(name = "age") int age,
+                      @RequestParam(name = "email") String email,
                       @RequestParam(name = "password") String password,
                       @RequestParam(name = "roleName") String roleName) {
         User user = new User();
-        user.setUsername(username);
+        user.setEmail(email);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setAge(age);
         user.setPassword(password);
         user.addRole(roleService.getByName(roleName));
         userService.add(user);
-        return "redirect:/admin/allUsers";
+        return "redirect:/admin/";
     }
 
 
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
     public String deleteUser(@RequestParam("id") long id) {
         userService.delete(id);
-        return "redirect:/admin/allUsers";
+        return "redirect:/admin/";
     }
 
-    @RequestMapping(value = "/edit")
-    public String editUser(@RequestParam("id") long id, Model model) {
-        User user = userService.getById(id);
-        model.addAttribute("user", user);
-        model.addAttribute("roles", roleService.getAllRoles());
-        return "/admin/edit";
-    }
-
-    @RequestMapping(value = "/update/{id}")
-    public String updateUser(@PathVariable("id") long id,
-                             @RequestParam(name = "username") String username,
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String updateUser(@RequestParam(name = "id") long id,
+                             @RequestParam(name = "firstName") String firstName,
+                             @RequestParam(name = "lastName") String lastName,
+                             @RequestParam(name = "age") int age,
+                             @RequestParam(name = "email") String email,
                              @RequestParam(name = "password") String password,
                              @RequestParam(name = "roleName") String roleName) {
         User user = userService.getById(id);
-        user.setUsername(username);
+        user.setEmail(email);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setAge(age);
         user.setPassword(password);
         user.addRole(roleService.getByName(roleName));
         userService.edit(user);
-        return "redirect:/admin/allUsers";
+        return "redirect:/admin/";
     }
 }
